ea202e13f542b840985c6a0d13e522a0db907f68
Maura Small <maurasmall@creighton.edu>
2021-09-29 17:49:47 -0500

    added new controls

---
+++ forms/newControls/newControls.js
@@ -0,0 +1,15 @@
+/*
+New Control: Slider
+Changed slider color to green and slider from round to square
+New Control: Switch 
+Chnanged switch default ot False and changed text to "True or False"
+New Control: Hamburger
+Changed border color to #00FF00 and changed alignment from right to left 
+New Control: Pagnigation 
+Chnaged font styel and size 
+*/
+
+
+butnSave.onclick=function(){
+  console.log("Saved My Changes")
+}
